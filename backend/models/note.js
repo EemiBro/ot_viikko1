79bc96b022b328bdf7cfd8b91784741eb3843d78
Eemeli Pohjonen <eemelipohjonen@kamk.fi>
2026-09-12 19:38:31 +0300

    osa4

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,18 +1,5 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-mongoose.set('strictQuery', false)
-
-console.log('connecting to', url)
-mongoose.connect(url, { family: 4 })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
